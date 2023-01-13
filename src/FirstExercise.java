b0b47466624138647d7e0f36aa2c696c072fe926
Marcin Wróblewski <marcin.boby@gmail.com>
2023-01-13 09:42:48 +0100

    first exercise

---
+++ src/FirstExercise.java
@@ -0,0 +1,11 @@
+
+public class FirstExercise {
+    /*
+    1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Marcin");
+    }
+}
