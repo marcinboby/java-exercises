4e706fe840f4e326bfc73c90672f2f3b21b28589
Marcin Wróblewski <marcin.boby@gmail.com>
2023-01-22 18:46:04 +0100

    added new exercise (2)

---
+++ src/exercise2/Exercise2.java
@@ -0,0 +1,18 @@
+package exercise2;
+
+/**
+ *  Write a Java program to print the sum of two numbers.
+ * Test Data:
+ * 74 + 36
+ * Expected Output :
+ * 110
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        int number1 = 74;
+        int number2 = 36;
+        int sum;
+        sum = number1 + number2;
+        System.out.println("Result of adding two numbers(" + number1 + ", " + number2 + ") is: " + sum);
+    }
+}
