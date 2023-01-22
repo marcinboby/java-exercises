d948e5f16e1ba10b39cfdf21c3c9c649a416b95f
Marcin Wróblewski <marcin.boby@gmail.com>
2023-01-22 22:38:46 +0100

    add new exercise

---
+++ src/exercise003/Exercise3.java
@@ -0,0 +1,16 @@
+package exercise003;
+
+/**
+ * Write a Java program to divide two numbers and print on the screen.
+ * Test Data :
+ * 50/3
+ * Expected Output :
+ * 16
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        int number1 =50, number2 = 3;
+        double result= number1/number2;
+        System.out.println("Result of dividing two numbers(" + number1+ ", " + number2 + ") is: " + result);
+    }
+}
