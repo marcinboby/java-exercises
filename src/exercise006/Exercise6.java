54d4dff29020b121aa60cf6ef607ac0ad6a32ba3
Marcin Wróblewski <marcin.boby@gmail.com>
2023-01-23 20:30:16 +0100

    add new exercise(6)

---
+++ src/exercise006/Exercise6.java
@@ -0,0 +1,26 @@
+package exercise006;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers
+ */
+public class Exercise6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        Double firstNumber = scanner.nextDouble();
+        System.out.print("Input second number: ");
+        Double secNumber = scanner.nextDouble();
+        double sum = firstNumber + secNumber;
+        System.out.println("Sum of those numbers is: " + sum);
+        double diff = firstNumber - secNumber;
+        System.out.println("Difference between those numbers is :" + diff);
+        double product = firstNumber * secNumber;
+        System.out.println("Product of those numbers is: " + product);
+        double quotient = firstNumber / secNumber;
+        System.out.println("Quotient of those numbers is: " + quotient);
+        double reminder = firstNumber % secNumber;
+        System.out.println("Reminder of those numbers is: " + reminder);
+    }
+}
