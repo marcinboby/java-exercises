5eff1d65c6db92c4d92a496242d2804687381b94
Marcin Wróblewski <marcin.boby@gmail.com>
2023-01-24 18:32:02 +0100

    add new exercise(10)

---
+++ src/exercise010/Exercise10.java
@@ -0,0 +1,21 @@
+package exercise010;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program to multiply two binary numbers.
+ */
+public class Exercise10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input first binary number: ");
+        int firstNumber = scanner.nextInt(2);
+        System.out.print("Input second binary number: ");
+        int secondNumber = scanner.nextInt(2);
+        int sum;
+        sum = firstNumber * secondNumber;
+        String binarySum = Integer.toString(sum, 2);
+        System.out.println("Result of multiplication two binaries is: " + binarySum);
+    }
+}
+
